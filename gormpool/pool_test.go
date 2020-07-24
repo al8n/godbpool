73ceae92644d6db367488b5fa6aaae212a2ba8c4
ALiuGuanyan <13136386067@163.com>
2020-07-24 16:29:24 +0800

    update test case

--- gormpool/pool_test.go
+++ gormpool/pool_test.go
@@ -27,7 +27,7 @@ func TestMySQLNewPool(t *testing.T) {
 	var wg sync.WaitGroup
 	for i := 0; i < 10; i++ {
 		wg.Add(1)
-		go mockJob(&wg, p, 5*time.Second, i)
+		go mockJob(&wg, p, 5*time.Second)
 	}
 	wg.Wait()
 	canc()
@@ -51,36 +51,25 @@ func TestMySQLClose(t *testing.T) {
 	var wg sync.WaitGroup
 	for i := 0; i < 2; i++ {
 		wg.Add(1)
-		go mockJob(&wg, p, 2*time.Second, i)
+		go mockJob(&wg, p, 2*time.Second)
 	}
 
 	for i := 0; i < 2; i++ {
 		wg.Add(1)
-		go mockJob(&wg, p, 8*time.Second, i+2)
+		go mockJob(&wg, p, 8*time.Second)
 	}
 	time.Sleep(4 * time.Second)
 	canc()
 	wg.Wait()
 }
 
-func mockJob(wg *sync.WaitGroup, p *Pool, duration time.Duration, idx int) {
-	fmt.Printf("Routine %d begin\n", idx)
-	format(idx, p.IdleConn(), "Idle")
-	format(idx, p.BusyConn(), "Busy")
+func mockJob(wg *sync.WaitGroup, p *Pool, duration time.Duration) {
 	conn, err := p.Get()
 	if err != nil {
 		fmt.Println(err)
 	} else {
-		format(idx, p.IdleConn(), "Idle")
-		format(idx, p.BusyConn(), "Busy")
 		time.Sleep(duration)
 		p.Put(conn)
-		format(idx, p.IdleConn(), "Idle")
-		format(idx, p.BusyConn(), "Busy")
 	}
 	wg.Done()
-}
-
-func format(idx int, value uint64, typ string) {
-	fmt.Printf("Routine %d: %s: %d\n", idx, typ, value)
 }
