8d6898ba58297a21d9097acde9d5fab1ec7f5923
ALiuGuanyan <13136386067@163.com>
2020-07-24 16:49:09 +0800

    update test shell

--- test.sh
+++ test.sh
@@ -1,7 +1,13 @@
 set -e
 echo "" > coverage.txt
 
-cd ./gormpool
-./test.sh
+set -e
 
-cd ../
+for d in $(go list ./...); do
+	echo $d
+    go test -v -race -coverprofile=profile.out -covermode=atomic $d
+    if [ -f profile.out ]; then
+        cat profile.out >> ../coverage.txt
+        rm profile.out
+    fi
+done
